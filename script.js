883e38da6fd26574fb81d5b7b065a549aec1792c
FesetoImre5 <feseto.imre@diak.szbi-pg.hu>
2024-10-17 13:17:11 +0200

    Navigation
    Nearing done

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-// Add an event listener to the window's load event
-window.addEventListener('load', function() {
-  // Create a new text node
-  const textNode = document.createTextNode('Text inserted on window load');
-
-  // Append the text node to the body
-  document.body.appendChild(textNode);
-});
+window.addEventListener('resize' , () => {
+    let width = window.innerWidth;
+    if(width < 700){
+        document.getElementById('navBar').style.display = 'none';
+        document.getElementById('openArrow').style.display = 'block';
+    }else{
+        document.getElementById('navBar').style.display = 'flex';
+        document.getElementById('openArrow').style.display = 'none';
+    }
+})
